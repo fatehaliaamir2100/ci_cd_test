9e6a116c27673db84e782e93641ebbe4f3872751
Fateh Ali Aamir <fateh.ali@dubizzlelabs.com>
2024-12-04 16:14:37 +0500

    testing fix

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello, CI/CD!'));
+app.get('/', (req, res) => {
+    res.send('Hello, CI/CD!');
+});
 
-app.listen(3000, () => console.log('App running on port 3000'));
+module.exports = app;
 
-// Added a comment
+if (require.main === module) {
+    app.listen(3000, () => {
+        console.log('Server is running on http://localhost:3000');
+    });
+}
--- test/index.test.js
+++ test/index.test.js
@@ -4,4 +4,5 @@ const app = require('../index');
 test('GET / should return Hello, CI/CD!', async () => {
     const response = await request(app).get('/');
     expect(response.text).toBe('Hello, CI/CD!');
+    expect(response.statusCode).toBe(200);
 });
